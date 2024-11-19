5ff014f7cee5aaec1d65f7c288142196ce3ab4b6
JSerrano01 <jaime.serrano2001@gmail.com>
2024-11-18 22:20:47 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import ValentinesInvitation from './components/ValentinesInvitation'; // Importa el componente Invitation
+// import './App.css';  // Si tienes un archivo global de estilos
+
+function App() {
+  return (
+    <div className="App">
+      <ValentinesInvitation />
+    </div>
+  );
+}
+
+export default App;
